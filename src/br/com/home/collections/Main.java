f674a702053c8c9896929dd125fab93c22281005
RodResq <rodrigoresque@yahoo.com.br>
2021-10-25 23:49:34 -0300

    revisao ArrayLists

--- src/br/com/home/collections/Main.java
+++ src/br/com/home/collections/Main.java
@@ -1,4 +1,17 @@
 package br.com.home.collections;
 
+import br.com.home.models.Pessoa;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        List<Pessoa> pessoas = new ArrayList<>();
+        pessoas.add(new Pessoa(1, "Pessoa 1"));
+        pessoas.add(new Pessoa(2, "Pessoa 2"));
+        System.out.println(pessoas.toString());
+    }
 }
