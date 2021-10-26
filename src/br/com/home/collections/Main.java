b7fce0e019a28a955eeebc871e690a2ffba1e610
RodResq <rodrigoresque@yahoo.com.br>
2021-10-26 15:51:46 -0300

    usando o removeIf do java 8! INTERRESSANTE

--- src/br/com/home/collections/Main.java
+++ src/br/com/home/collections/Main.java
@@ -3,15 +3,41 @@ package br.com.home.collections;
 import br.com.home.models.Pessoa;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Pessoa> pessoas = new ArrayList<>();
+        List<Pessoa> pessoas = new LinkedList<>();
         pessoas.add(new Pessoa(1, "Pessoa 1"));
         pessoas.add(new Pessoa(2, "Pessoa 2"));
-        System.out.println(pessoas);
+        pessoas.add(new Pessoa(3, "Pessoa 3"));
+        System.out.println("Com for");
+        for (int i = 0; i < pessoas.size(); i++) {
+            Pessoa p = pessoas.get(i);
+            System.out.println(p);
+        }
+
+        System.out.println("Com Iterator");
+        Iterator<Pessoa> iteratorPessoa = pessoas.iterator();
+//        while (iteratorPessoa.hasNext()) {
+//            Pessoa p = iteratorPessoa.next();
+//            if(p.getId() == 1) {
+//                iteratorPessoa.remove();
+////                pessoas.remove(p);
+//            }
+//            System.out.println(p);
+//        }
+        pessoas.removeIf(p -> p.getId() == 1);
+        // O java ultiliza os iterators por baixo dos panos
+        System.out.println("Com for-each");
+        for (Pessoa p: pessoas) {
+            System.out.println(p);
+        }
+
+        System.out.println("FIM!");
     }
 }
