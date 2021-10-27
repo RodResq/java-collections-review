f24c3f4ddf4d7c3d57dbc0aa9e8cdbf3ddd9696e
RodResq <rodrigoresque@yahoo.com.br>
2021-10-26 21:19:31 -0300

    PriorityQueuo recebendo um expressao lambida no seu interior. INTERRESSANTE

--- src/br/com/home/collections/MainQueue.java
+++ src/br/com/home/collections/MainQueue.java
@@ -8,10 +8,12 @@ import java.util.Queue;
 public class MainQueue {
 
     public static void main(String[] args) {
-        Queue<Pessoa> queue = new PriorityQueue<>();
-        queue.add(new Pessoa(3, "pessoa 3"));
-        queue.add(new Pessoa(1, "pessoa 1"));
-        queue.add(new Pessoa(2, "pessoa 2"));
+        Queue<Pessoa> queue = new PriorityQueue<Pessoa>((p1, p2) -> {
+            return p1.getNome().compareTo(p2.getNome());
+        });
+        queue.add(new Pessoa(3, "Mauro 3"));
+        queue.add(new Pessoa(1, "Cassia 1"));
+        queue.add(new Pessoa(2, "Abel 2"));
         System.out.println(queue);
         while (!queue.isEmpty()) {
             Pessoa p = queue.poll();
