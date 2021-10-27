59a9bffbdcaa72582b8eb5d9189d7d80ef376ece
RodResq <rodrigoresque@yahoo.com.br>
2021-10-26 21:02:13 -0300

    Conhecendo a interfece NavigableSet no qual o treeSet a implementa,INTERRESSANTE

--- src/br/com/home/collections/MainSet.java
+++ src/br/com/home/collections/MainSet.java
@@ -7,16 +7,19 @@ import java.util.*;
 public class MainSet {
 
     public static void main(String[] args) {
-        Set<Pessoa> set = new TreeSet<Pessoa>((p1, p2) -> {
+        NavigableSet<Pessoa> set = new TreeSet<Pessoa>((p1, p2) -> {
             return p1.getNome().compareTo(p2.getNome());
         });
         set.add(new Pessoa(2, "Beka 2"));
         set.add(new Pessoa(3, "Pessoa 3"));
         set.add(new Pessoa(1, "Abel 1"));
+        set.add(new Pessoa(4, "Pessoa 4"));
+        Pessoa teste = set.lower(new Pessoa(2, "Beka 2"));
+        System.out.println(teste);
 //        set.add(null);
-        System.out.println(set);
-        for (Pessoa p: set) {
-            System.out.println(p);
-        }
+//        System.out.println(set);
+//        for (Pessoa p: set) {
+//            System.out.println(p);
+//        }
     }
 }
