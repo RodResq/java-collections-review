baccfecae1855192c9a184a0ea09c8bc8a3d5166
RodResq <rodrigoresque@yahoo.com.br>
2021-10-27 17:22:25 -0300

    revisao vetores!

--- src/br/com/home/estruturadados/main/MainEd.java
+++ src/br/com/home/estruturadados/main/MainEd.java
@@ -9,14 +9,28 @@ public class MainEd {
     public static void main(String[] args) {
         System.out.println("Digite a opcao desejada: ");
         System.out.println("1 - Gerenciamento de Memoria");
+        System.out.println("2 - Vetor");
         Scanner scanner = new Scanner(System.in);
         int opcao = scanner.nextInt();
         switch (opcao) {
             case 1:
                 fazerGerenciamentoMemoria();
                 break;
+            case 2:
+                fazerVetor();
+                break;
         }
         scanner.close();
+    }
+
+    private static void fazerVetor() {
+        PessoaEd[] vetorPessoas = new PessoaEd[3];
+        vetorPessoas[0] = new PessoaEd(1, "Pessoa 1 no Vetor");
+        System.out.println(vetorPessoas[0].getNome());
+        System.out.println(vetorPessoas[1]);
+        int[] vetorInteiros = new int[3];
+        System.out.println(vetorInteiros[0]);
+        System.out.println(vetorInteiros[3]);
     }
 
     private static void fazerGerenciamentoMemoria() {
