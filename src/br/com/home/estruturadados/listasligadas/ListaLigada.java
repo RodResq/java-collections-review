2a56f3dbf7802ab40582cfff9bcb8b2846d2e901
RodResq <rodrigoresque@yahoo.com.br>
2021-10-28 16:39:28 -0300

    revisao ListasLigadas!

---
+++ src/br/com/home/estruturadados/listasligadas/ListaLigada.java
@@ -0,0 +1,13 @@
+package br.com.home.estruturadados.listasligadas;
+
+public class ListaLigada<T> {
+
+    private No<T> primeiroNo;
+    private No<T> ultimoNo;
+    private int tamanho;
+
+    public ListaLigada() {
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+    }
+}
