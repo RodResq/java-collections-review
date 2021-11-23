53fb45bab528e6ee9da7d51f103ecde5e344957a
RodResq <rodrigoresque@yahoo.com.br>
2021-11-22 22:10:59 -0300

    linkedhashMap - review - garante ordem de insercao

--- src/br/com/home/collections/MainMap.java
+++ src/br/com/home/collections/MainMap.java
@@ -3,17 +3,23 @@ package br.com.home.collections;
 import br.com.home.models.Pessoa;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MainMap {
 
     public static void main(String[] args) {
-        Map<String, Pessoa> mapaPessoas = new HashMap<>();
+        Map<String, Pessoa> mapaPessoas = new LinkedHashMap<>(16, 0.75f, true);
         System.out.println(mapaPessoas.put("legal", new Pessoa(1, "pessoa 1")));
         System.out.println(mapaPessoas);
         System.out.println(mapaPessoas.put("legal", new Pessoa(2, "pessoa 2")));
         System.out.println(mapaPessoas);
         mapaPessoas.put(null, new Pessoa(-1, "NuLO"));
+        System.out.println(mapaPessoas);
+        mapaPessoas.put("teste", null);
+        System.out.println(mapaPessoas);
+        Pessoa p = mapaPessoas.get("legal");
+        System.out.println(p);
         System.out.println(mapaPessoas);
     }
 }
