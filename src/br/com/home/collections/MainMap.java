43f97ff57ac13c783aa570a9eefa3d87b768a0d7
RodResq <rodrigoresque@yahoo.com.br>
2021-11-22 22:19:24 -0300

    treeMap - review - ordem natural

--- src/br/com/home/collections/MainMap.java
+++ src/br/com/home/collections/MainMap.java
@@ -2,24 +2,24 @@ package br.com.home.collections;
 
 import br.com.home.models.Pessoa;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MainMap {
 
     public static void main(String[] args) {
-        Map<String, Pessoa> mapaPessoas = new LinkedHashMap<>(16, 0.75f, true);
+        Map<String, Pessoa> mapaPessoas = new TreeMap<String, Pessoa>(Comparator.reverseOrder());
         System.out.println(mapaPessoas.put("legal", new Pessoa(1, "pessoa 1")));
         System.out.println(mapaPessoas);
         System.out.println(mapaPessoas.put("legal", new Pessoa(2, "pessoa 2")));
         System.out.println(mapaPessoas);
-        mapaPessoas.put(null, new Pessoa(-1, "NuLO"));
+//        mapaPessoas.put(null, new Pessoa(-1, "NuLO"));
         System.out.println(mapaPessoas);
         mapaPessoas.put("teste", null);
         System.out.println(mapaPessoas);
         Pessoa p = mapaPessoas.get("legal");
         System.out.println(p);
+        System.out.println(mapaPessoas);
+        mapaPessoas.put("a", new Pessoa(100, "Pessoa a"));
         System.out.println(mapaPessoas);
     }
 }
